cca773bf10ac6554f6bf8c885d78ebf8855fc741
tariangggraini <tarianggrainimanis@gmail.com>
2026-06-13 17:30:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../../frontend')));
 
-app.get('/api/test-db', (req, res) => {
-  db.query('SELECT 1', (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.json({ message: 'Database terkoneksi dengan sukses!' });
-  });
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
+res.sendFile(path.join(__dirname, '../../frontend/index.html'));
 
 app.listen(PORT, () => {
   console.log(`Server aktif di port ${PORT}`);
