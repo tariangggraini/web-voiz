8402fcd754579312d9a62f59ecd6e6b11b0a1d97
tariangggraini <tariangggraini@gmail.com>
2026-06-13 16:21:38 +0700

    fix: path frontend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-// backend/server.js
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const db = require('./config/db');
 
 const app = express();
@@ -9,25 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Contoh penggunaan di rute
-app.get('/api/test-db', (req, res) => {
-  db.query('SELECT 1', (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.json({ message: 'Database terkoneksi dengan sukses!' });
-  });
-});
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Voiz API Server Aktif!</h1><p>Gunakan /api/... untuk mengakses endpoint.</p>');
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
-
-// PENTING: Jika error 'index.html' muncul, pastikan Anda punya folder 'public'
-// Jika tidak ada, jangan gunakan app.use(express.static)
-// app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Server aktif di port ${PORT}`);
